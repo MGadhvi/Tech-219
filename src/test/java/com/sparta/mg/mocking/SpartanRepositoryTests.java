337ef3b7f63ba7276d7738067009851f8d41231f
Manish Gadhvi <mgadhvi@spartaglobal.com>
2023-05-17 11:19:31 +0100

    added mocking code for demonstration

--- src/test/java/com/sparta/mg/mocking/SpartanRepositoryTests.java
+++ src/test/java/com/sparta/mg/mocking/SpartanRepositoryTests.java
@@ -1,8 +1,6 @@
 package com.sparta.mg.mocking;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -13,35 +11,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.inOrder;
 
 public class SpartanRepositoryTests {
+    //BeforeAll - setupAll() - static
+    //BeforeEach - setup()
+    //AfterAll - tearDownAll() - static
+    //AfterEach - tearDown()
     Spartan mockSpartan;
     Spartan spySpartan;
 
+    @BeforeAll
+    static void setupAll(TestInfo testInfo) {
+        System.out.println("BeforeAll");
+        System.out.println(testInfo.getTestClass() + "Started");
+    }
+
     @BeforeEach
-    void setup() {
-        mockSpartan=Mockito.mock(Spartan.class);
-        Spartan spartan=new Spartan(1,"Manish","Java",LocalDate.now());
-        spySpartan=Mockito.spy(spartan); //partial Mock
+    void setup(TestInfo testInfo) {
+        System.out.println("BeforeEach");
+        System.out.println(testInfo.getDisplayName() + "Started");
+        mockSpartan = Mockito.mock(Spartan.class);
+        Spartan spartan = new Spartan(1, "Manish", "Java", LocalDate.now());
+        spySpartan = Mockito.spy(spartan); //partial Mock
+
     }
 
     @Test
     @DisplayName("Check that the repository can print all Spartans")
     void checkThatTheRepositoryCanPrintAllSpartans() {
-        Spartan spartan = new Spartan(12, "Manish","Java", LocalDate.now());
+        Spartan spartan = new Spartan(12, "Manish", "Java", LocalDate.now());
         Spartan mySpartan = Mockito.mock(Spartan.class);
         System.out.println(mySpartan.getClass());
     }
 
     @Test
     @DisplayName("Testing Spartan Repository Print Method")
-    void testingSpartanRepositoryPrintMethod(){
+    void testingSpartanRepositoryPrintMethod() {
         SpartanRepository.addSpartan(mockSpartan);
+        SpartanRepository.addSpartan(spySpartan);
         Mockito.when(mockSpartan.toString()).thenReturn("Found Mock Spartan");
-        assertEquals("Found Mock Spartan"+"\n",SpartanRepository.getAllSpartans());
+        Mockito.doReturn("Found Mock Spartan").when(mockSpartan.toString());
+        assertEquals("Found Mock Spartan" + "\n", SpartanRepository.getAllSpartans());
     }
 
     @Test
     @DisplayName("Testing Method Order")
-    void testingMethodOrder(){
+    void testingMethodOrder() {
         Mockito.when(mockSpartan.getStartDate())
                 .thenReturn(LocalDate.now())
                 .thenReturn(LocalDate.MAX);
@@ -52,10 +65,10 @@ public class SpartanRepositoryTests {
 
     @Test
     @DisplayName("Check that the getId method is called on our mock")
-    void checkThatTheGetIdMethodIsCalledOnOurMock(){
+    void checkThatTheGetIdMethodIsCalledOnOurMock() {
         SpartanRepository.addSpartan(mockSpartan);
         boolean present = SpartanRepository.findSpartan(1).isPresent();
-        Mockito.verify(mockSpartan,Mockito.times(1)).getId();
+        Mockito.verify(mockSpartan, Mockito.times(1)).getId();
     }
 
     @Test
@@ -68,8 +81,20 @@ public class SpartanRepositoryTests {
         InOrder inOrder = inOrder(mockSpartan);
         inOrder.verify(mockSpartan).setName("Manish");
         inOrder.verify(mockSpartan).setCourse("C#");
-        inOrder.verify(mockSpartan).getName();
         inOrder.verify(mockSpartan).getCourse();
+        inOrder.verify(mockSpartan).getName();
+    }
+
+    @AfterEach
+    void tearDown(TestInfo testInfo) {
+        System.out.println("AfterEach");
+        System.out.println(testInfo.getDisplayName() + "Finished");
+    }
+
+    @AfterAll
+    static void teardownAll(TestInfo testInfo) {
+        System.out.println("AfterAll");
+        System.out.println(testInfo.getTestClass() + "Finished");
     }
 
 }
